6634aa99efe29a2d82e7b302e7397c1b5b7492f7
zbsmobile <zbsmobile@gmail.com>
2026-09-22 19:17:37 -0400

    moved validation to a new func

--- GuessingNumbers.py
+++ GuessingNumbers.py
@@ -1,6 +1,20 @@
 """This program does stuff. I'll fill this in later. I already wrote the other assignment about what I'm going to do"""
 from random import randint
 
+def validation(): #Created to not go more than 3 deep
+    validGuess = False
+    while validGuess != True: #Make them keep trying until they get it right
+                guess=input("Enter your next guess")
+                if guess.isdigit(): #Weeding out nonnumerical inputs and negative numbers so no errors occur
+                    if int(guess)==round(int(guess)): #Weeding out decimal points so nothing weird happens
+                        validGuess=True
+                    else:
+                        print("Your guess must be an integer")
+                else:
+                    print("Your guess must be a number")
+    return guess
+
+
 def round ():
     low=0 #Just making low and high in case I want to change it later
     high=100
@@ -14,13 +28,7 @@ def round ():
     guess=high+1 #This is so it is always outside of the range
 
     while guess != a:
-        validGuess = False
-        while validGuess != True: #Make them keep trying until they get it right
-            guess=input("Enter your next guess")
-            if guess.isdigit(): #Weeding out nonnumerical inputs and negative numbers so no errors occur
-                if int(guess)==round(int(guess)): #Weeding out decimal points so nothing weird happens
-                    validGuess=True
-                else:
-                    print("Your guess must be an integer")
-            else:
-                print("Your guess must be a number")
+         guess = validation()
+
+        
+        
